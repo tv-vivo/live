b63e4995c7faa62755ee6e3f5bb003ed07efcdc6
tv-vivo <botandresvpn@gmail.com>
2024-07-16 18:17:04 -0500

    Update config.js

--- movies/config.js
+++ movies/config.js
@@ -4,6 +4,8 @@ const getVideoUrl = (file) => {
             return "https://vpn-movie-ofc.blogspot.com/2024/05/embed_27.html?r=aHR0cHM6Ly9zd2R5dS5jb20vZS8yamdiNzVrMjhjNmw=";
         case "2":
             return "https://vpn-movie-ofc.blogspot.com/2024/05/embed_27.html?r=aHR0cHM6Ly9zdHJ3aXNoLmNvbS9lLzJhYzNhbDVobDNwYw=="
+        case "3":
+            return "https://vpn-movie-ofc.blogspot.com/2024/05/embed_27.html?r=aHR0cHM6Ly9zdHJ3aXNoLmNvbS9lL2w4MGw4Zms3ZTduZg=="
         default:
             return file;
     }
@@ -26,6 +28,15 @@ const movies = {
             id: 1,
             image: "https://media.themoviedb.org/t/p/w220_and_h330_face/xmbU4JTUm8rsdtn7Y3Fcm30GpeT.jpg",
             url: "https://vpnmovie.onrender.com/movies/reproductor?id=2"
+        }
+    },
+    3: {
+        title: "Tarot (2024)",
+        json: {
+            title: "Tarot (2024)",
+            id: 1,
+            image: "https://media.themoviedb.org/t/p/w220_and_h330_face/xmbU4JTUm8rsdtn7Y3Fcm30GpeT.jpg",
+            url: "https://vpnmovie.onrender.com/movies/reproductor?id=3"
         }
     },
 };
