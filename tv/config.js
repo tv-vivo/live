39f5d73a6a448cb4b2249db0480b0e6251ca16cf
tv-vivo <botandresvpn@gmail.com>
2024-07-15 20:54:38 -0500

    Update config.js

--- tv/config.js
+++ tv/config.js
@@ -3,7 +3,9 @@ const getVideoUrl = (file) => {
         case "sñlcol":
             return "https://streaming.rtvc.gov.co/TV_Senal_Colombia_live/smil:live.smil/playlist.m3u8?PlaylistM3UCL";
         case "orbittv":
-            return "https://ss3.domint.net:3134/otv_str/orbittv/playlist.m3u8?PlaylistM3UCL";
+          return "https://ss3.domint.net:3134/otv_str/orbittv/playlist.m3u8?PlaylistM3UCL";
+        case "disneych": 
+            return "https://spa-ha-p005.cdn.masmediatv.es/SVoriginOperatorEdge2/smil:21_HD.smil/manifest.m3u8"
         default:
             return file;
     }
@@ -24,6 +26,14 @@ const movies = {
             title: "Orbit Tv",
             id: "orbittv",
             url: "vpnmovie.onrender.com/tv/reproductor?id=orbittv"
+        }
+    },
+    3: {
+        title: "Disney Chanel",
+        json: {
+            title: "Disney Chanel",
+            id: "disneych",
+            url: "vpnmovie.onrender.com/tv/reproductor?id=disneych"
         }
     },
 };
