45d1f6cc1a28be329d0bfcaebf7ce29994b17750
tv-vivo <botandresvpn@gmail.com>
2024-07-17 18:34:39 -0500

    Update config.js

--- tv/config.js
+++ tv/config.js
@@ -2,7 +2,7 @@ const dominio = "https://vpnmovie.onrender.com/tv/"
 const vpn_movie = (file) => {
     switch (file) {
         case "bobesponja":
-            return "https://service-stitcher.clusters.pluto.tv/v1/stitch/embed/hls/channel/5f1aca0b4e448e00075e7c5elivestitch/master.m3u8?advertisingId=channel&appName=rokuchannel&appVersion=1.0&bmodel=bm1&content=channel&content_rating=ROKU_ADS_CONTENT_RATING&content_type=livefeed&coppa=false&deviceDNT=1&deviceId=channel&deviceMake=rokuChannel&deviceModel=web&deviceType=rokuChannel&deviceVersion=1.0&embedPartner=rokuChannel&is_lat=1&platform=web&rdid=channel"
+            return "singId=channel&appName=rokuchannel&appVersion=1.0&bmodel=bm1&content=channel&content_rating=ROKU_ADS_CONTENT_RATING&content_type=livefeed&coppa=false&deviceDNT=1&deviceId=channel&deviceMake=rokuChannel&deviceModel=web&deviceType=rokuChannel&deviceVersion=1.0&embedPartner=rokuChannel&is_lat=1&platform=web&rdid=channel"
         case "pdmagicos":
             return "https://jmp2.uk/PlutoTV/6322099c822bbc00074857db-alt.m3u8"
         case "sñlcol":
@@ -23,6 +23,15 @@ const vpn_movie = (file) => {
 };
 
 const movies = {
+    8: {
+        title: "BOB ESPONJA 24/7",
+        json: {
+            title: "BOB ESPONJA 24/7",
+            id: "bobesponja",
+            image: "https://telegra.ph/file/d027989e7506f3f4a60f3.jpg",
+            url: dominio + "reproductor2?id=bobesponja"
+        }
+    },
     7: {
         title: "LOS PADRINOS MAGICOS 24/7",
         json: {
