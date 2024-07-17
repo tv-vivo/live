0d409d16bc92d9dfd06f2be26d2e3a53968650ab
tv-vivo <botandresvpn@gmail.com>
2024-07-17 14:15:49 -0500

    Update config.js

--- movies/config.js
+++ movies/config.js
@@ -1,10 +1,9 @@
 const embed = "https://vpn-movie-ofc.blogspot.com/2024/05/embed_27.html?r="
 const dominio = "https://vpnmovie.onrender.com/movies/reproductor?id="
+const vpn_movie = require("./config2.js")
 const getVideoUrl = (file) => {
     return vpn_movie(file, "https://vpn-movie-ofc.blogspot.com/2024/05/embed_27.html?r=")
 }
-const vpn_movie = (file, embed) => {
-
 const movies = {
     11: {
         title: "Bad Boys: Corre o muere (2024)",
