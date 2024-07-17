728d3678e0f90728584a54f174fc013b42c9d253
tv-vivo <botandresvpn@gmail.com>
2024-07-16 22:35:56 -0500

    Update config.js

--- movies/config.js
+++ movies/config.js
@@ -1,13 +1,17 @@
+const embed = "https://vpn-movie-ofc.blogspot.com/2024/05/embed_27.html?r="
+const dominio = "https://vpnmovie.onrender.com/movies/reproductor?id="
 const getVideoUrl = (file) => {
     switch (file) {
+        case "5":
+            return embed + "aHR0cHM6Ly9zdHJ3aXNoLmNvbS9lLzR6cGplNXZudmpsYg=="
         case "1":
-            return "https://vpn-movie-ofc.blogspot.com/2024/05/embed_27.html?r=aHR0cHM6Ly9zd2R5dS5jb20vZS8yamdiNzVrMjhjNmw=";
+            return embed + "aHR0cHM6Ly9zd2R5dS5jb20vZS8yamdiNzVrMjhjNmw=";
         case "2":
-            return "https://vpn-movie-ofc.blogspot.com/2024/05/embed_27.html?r=aHR0cHM6Ly9zdHJ3aXNoLmNvbS9lLzJhYzNhbDVobDNwYw=="
+            return embed + "aHR0cHM6Ly9zdHJ3aXNoLmNvbS9lLzJhYzNhbDVobDNwYw=="
         case "3":
-            return "https://vpn-movie-ofc.blogspot.com/2024/05/embed_27.html?r=aHR0cHM6Ly9zdHJ3aXNoLmNvbS9lL2w4MGw4Zms3ZTduZg=="
+            return embed + "aHR0cHM6Ly9zdHJ3aXNoLmNvbS9lL2w4MGw4Zms3ZTduZg=="
         case "4":
-            return "https://vpn-movie-ofc.blogspot.com/2024/05/embed_27.html?r=aHR0cHM6Ly9zdHJ3aXNoLmNvbS9lL2g5NGg2MjltaDIwNg=="
+            return embed + "aHR0cHM6Ly9zdHJ3aXNoLmNvbS9lL2g5NGg2MjltaDIwNg=="
         default:
             return file;
     }
@@ -20,7 +24,7 @@ const movies = {
             title: "Un Lugar En Silencio (2024)",
             id: 1,
             image: "https://media.themoviedb.org/t/p/w220_and_h330_face/jGOi939xU8BDRfAAhN6eiTSf3H8.jpg",
-            url: "https://vpnmovie.onrender.com/movies/reproductor?id=1"
+            url: dominio + "1"
         }
     },
     2: {
@@ -29,7 +33,7 @@ const movies = {
             title: "Free Guy",
             id: 1,
             image: "https://media.themoviedb.org/t/p/w220_and_h330_face/xmbU4JTUm8rsdtn7Y3Fcm30GpeT.jpg",
-            url: "https://vpnmovie.onrender.com/movies/reproductor?id=2"
+            url: dominio + "2"
         }
     },
     3: {
@@ -38,7 +42,7 @@ const movies = {
             title: "Tarot (2024)",
             id: 1,
             image: "https://media.themoviedb.org/t/p/w220_and_h330_face/gAEUXC37vl1SnM7PXsHTF23I2vq.jpg",
-            url: "https://vpnmovie.onrender.com/movies/reproductor?id=3"
+            url: dominio + "3"
         }
     },
     4: {
@@ -47,7 +51,7 @@ const movies = {
             title: "Atlas (2024)",
             id: 1,
             image: "https://media.themoviedb.org/t/p/w220_and_h330_face/bcM2Tl5HlsvPBnL8DKP9Ie6vU4r.jpg",
-            url: "https://vpnmovie.onrender.com/movies/reproductor?id=4"
+            url: dominio + "4"
         }
     },
 };
