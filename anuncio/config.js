c48f7df76904d214dc8c90baf985e15d267911dc
tv-vivo <botandresvpn@gmail.com>
2024-09-11 20:24:16 -0500

    Create config.js

---
+++ anuncio/config.js
@@ -0,0 +1,32 @@
+const anuncios = {
+        anuncio1: {
+            URL: "https://vpnmovie.onrender.com/logo.mp4",
+            redireccion: "https://vpnmovie.onrender.com/",
+            titulo: "logo",
+            time: 2 // Tiempo en segundos para saltar
+        },
+        anuncio2: {
+            URL: "https://vpnmovie.onrender.com/logo.mp4",
+            redireccion: "https://vpnmovie.onrender.com/",
+            titulo: "anuncio2",
+            time: 5
+        },
+        anuncio3: {
+            URL: "https://vpnmovie.onrender.com/logo.mp4",
+            redireccion: "https://vpnmovie.onrender.com/",
+            titulo: "anuncio3",
+            time: 7
+        },
+        anuncio4: {
+            URL: "https://vpnmovie.onrender.com/logo.mp4",
+            redireccion: "https://vpnmovie.onrender.com/",
+            titulo: "anuncio4",
+            time: 10
+        },
+        anuncio5: {
+            URL: "https://vpnmovie.onrender.com/logo.mp4",
+            redireccion: "https://vpnmovie.onrender.com/",
+            titulo: "anuncio5",
+            time: 3
+        }
+    };
