110a8035e956bb4933ea585d4729f47ff86d9f97
tv-vivo <botandresvpn@gmail.com>
2024-09-13 18:03:41 -0500

    Update config.js

--- api/config.js
+++ api/config.js
@@ -7,5 +7,14 @@
                     mes: 9,
                     year: 2024
                 }
+            },
+            {
+                user: 'andresvpn',
+                key: '1088829889',
+                fechaVencimiento: {
+                    dia: 14,
+                    mes: 9,
+                    year: 2025
+                }
             }
         ];
