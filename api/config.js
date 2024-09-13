126f98c6853a40e1b66732b33a85b59833c6c36b
tv-vivo <botandresvpn@gmail.com>
2024-09-13 15:18:54 -0500

    Create config.js

---
+++ api/config.js
@@ -0,0 +1,11 @@
+        const usuarios = [
+            {
+                user: 'andres',
+                key: 'andres',
+                fechaVencimiento: {
+                    dia: 13,
+                    mes: 9,
+                    año: 2024
+                }
+            }
+        ];
