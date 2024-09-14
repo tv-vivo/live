45f75b24b69d34a157c826f6482febce84a752d6
tv-vivo <botandresvpn@gmail.com>
2024-09-14 15:30:42 -0500

    Update config.js

--- api/config.js
+++ api/config.js
@@ -21,7 +21,7 @@
                 user: '1234',
                 key: '1234',
                 fechaVencimiento: {
-                    dia: 14,
+                    dia: 16,
                     mes: 9,
                     year: 2024
                 }
