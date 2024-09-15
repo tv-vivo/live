2fe33176de9e19752e0d60428ae1f965bfeca720
tv-vivo <botandresvpn@gmail.com>
2024-09-15 12:28:26 -0500

    Update config.js

--- api/config.js
+++ api/config.js
@@ -1,5 +1,14 @@
         const usuarios = [
                 {
+                user: 'nicoll',
+                key: 'minie0510',
+                fechaVencimiento: {
+                    dia: 15,
+                    mes: 9,
+                    year: 2025
+                }
+            },
+                {
                 user: '12345',
                 key: '12345',
                 fechaVencimiento: {
