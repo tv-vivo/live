e9fa7c9f2e7efe1fcd66600282e324fab2ccbab3
tv-vivo <botandresvpn@gmail.com>
2024-09-24 18:04:13 -0500

    Delete games/subwaysurfers directory

--- games/subwaysurfers/texture.js
+++
@@ -1,5 +0,0 @@
-var texture =[];
-texture['base'] = loadTexture(gl, 'base.jpg');
-texture['rails'] = loadTexture(gl,'new_track.jpg');
-texture['wall'] = loadTexture(gl, 'BrickWall.jpg');
-texture['coin'] = loadTexture(gl, 'yellow.jpg');
