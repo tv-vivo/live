006718d7f8e4658d754fb6b33f28abb2b380e3f5
tv-vivo <botandresvpn@gmail.com>
2024-09-27 11:43:09 -0500

    Update config.js

--- anuncio/config.js
+++ anuncio/config.js
@@ -2,19 +2,7 @@
         {
             titulo: "¡Nuevo Producto Increíble!",
             video: "https://vpnmovie.onrender.com/logo.mp4",
-            url: "https://ejemplo.com/producto1",
+            url: "https://vpnmovie.onrender.com/download",
             time: 5
-        },
-        {
-            titulo: "Descubre nuestra última colección",
-            video: "https://vpnmovie.onrender.com/logo.mp4",
-            url: "https://ejemplo.com/coleccion",
-            time: 7
-        },
-        {
-            titulo: "Oferta Especial de Verano",
-            video: "https://vpnmovie.onrender.com/logo.mp4",
-            url: "https://ejemplo.com/oferta-verano",
-            time: 6
         }
     ];
