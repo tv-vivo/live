99f0f50d98daf8a93feed7c0ffa8d9da367af606
tv-vivo <botandresvpn@gmail.com>
2024-09-30 21:15:38 -0500

    Create notificaciones.js

---
+++ notificaciones.js
@@ -0,0 +1,7 @@
+const notifications = [
+ { title: "Shrek 4: Para Siempre", 
+message: "Pelicula Disponible",
+ time: "30/09/2024", 
+ imageUrl: "https://image.tmdb.org/t/p/w300//3MyKXb1ozCfq61tPNKpIggwvP6J.jpg", 
+ url: "go:home" }
+        ];
