c1d0e8ce7e9a0ea0102b2a1324459dcfa461b971
tv-vivo <botandresvpn@gmail.com>
2024-10-01 20:20:04 -0500

    Update premium.js

--- api/premium.js
+++ api/premium.js
@@ -2897,7 +2897,7 @@ const movies = [
   "titulo": "El cadáver de la novia",
   "imb": 3933,
   "img": "https://image.tmdb.org/t/p/w300/3ALM0VeZjGUryAqWo6pqohzbLDh.jpg",
-  "url": "https://vpn-movie-ofc.blogspot.com/2024/05/embed_27.html?r=aHR0cHM6Ly9wbGF5ZXJ3aXNoLmNvbS9lL3N0b2l1eG45Y3RkdA==024/05/embed_27.html?",
+  "url": "https://vpn-movie-ofc.blogspot.com/2024/05/embed_27.html?r=aHR0cHM6Ly9wbGF5ZXJ3aXNoLmNvbS9lL3N0b2l1eG45Y3RkdA==",
   "categoria": [
     "Romance",
     "Fantasía",
