ca64927c06297ffddf0b2be71dde8fe935cd9e58
tv-vivo <botandresvpn@gmail.com>
2024-10-01 21:01:13 -0500

    Update notificaciones.js

--- notificaciones.js
+++ notificaciones.js
@@ -1,13 +1,16 @@
 const notifications = [
-  { title: "Descendientes: el ascenso de Red", 
+  {
+title: "Descendientes: el ascenso de Red", 
 message: "Pelicula Disponible",
- time: "1/10/2024", 
- imageUrl: "https://lh3.googleusercontent.com/blogger_img_proxy/AEn0k_uouRoJ0vhUI64kuB1KWcL2aEEvPKn-u-SZv7Y7yXgEuA5EbQF30CGgHNgWHFX-pFSQMcDrPBPtu9olxWgAw792-NsqwHTa5h8_mle417NYmEtrh10jF9xRCcTpCdZOmn--XWw=w300
-", 
- url: "go:home" },
- { title: "Shrek 4: Para Siempre", 
+time: "1/10/2024", 
+imageUrl: "https://lh3.googleusercontent.com/blogger_img_proxy/AEn0k_uouRoJ0vhUI64kuB1KWcL2aEEvPKn-u-SZv7Y7yXgEuA5EbQF30CGgHNgWHFX-pFSQMcDrPBPtu9olxWgAw792-NsqwHTa5h8_mle417NYmEtrh10jF9xRCcTpCdZOmn--XWw=w300", 
+url: "go:home" 
+  },
+ {
+title: "Shrek 4: Para Siempre", 
 message: "Pelicula Disponible",
- time: "30/09/2024", 
- imageUrl: "https://image.tmdb.org/t/p/w300//3MyKXb1ozCfq61tPNKpIggwvP6J.jpg", 
- url: "go:home" }
+time: "30/09/2024", 
+imageUrl: "https://image.tmdb.org/t/p/w300//3MyKXb1ozCfq61tPNKpIggwvP6J.jpg", 
+url: "go:home" 
+ }
         ];
