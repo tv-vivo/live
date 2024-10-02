563ec52270ca0620dc15b313db58dcce3528d238
tv-vivo <botandresvpn@gmail.com>
2024-10-01 20:55:58 -0500

    Update notificaciones.js

--- notificaciones.js
+++ notificaciones.js
@@ -1,4 +1,10 @@
 const notifications = [
+  { title: "Descendientes: el ascenso de Red", 
+message: "Pelicula Disponible",
+ time: "1/10/2024", 
+ imageUrl: "https://lh3.googleusercontent.com/blogger_img_proxy/AEn0k_uouRoJ0vhUI64kuB1KWcL2aEEvPKn-u-SZv7Y7yXgEuA5EbQF30CGgHNgWHFX-pFSQMcDrPBPtu9olxWgAw792-NsqwHTa5h8_mle417NYmEtrh10jF9xRCcTpCdZOmn--XWw=w300
+", 
+ url: "go:home" },
  { title: "Shrek 4: Para Siempre", 
 message: "Pelicula Disponible",
  time: "30/09/2024", 
