5de313d9f79c3b84c1ca46b443dfbb4767593019
tv-vivo <botandresvpn@gmail.com>
2024-10-04 09:59:27 -0500

    Update notificaciones.js

--- notificaciones.js
+++ notificaciones.js
@@ -1,5 +1,12 @@
 const notifications = [
   {
+title: "Garfield: Fuera de casa", 
+message: "Pelicula Disponible",
+time: "4/10/2024", 
+imageUrl: "https://image.tmdb.org/t/p/w300/6QR2FOCQr41gSduN70WulRIhJb7.jpg", 
+url: "go:home" 
+  },
+  {
 title: "Descendientes: el ascenso de Red", 
 message: "Pelicula Disponible",
 time: "1/10/2024", 
